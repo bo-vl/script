91c92afc581141c3ae3ee292ef3959bab7550416
Bo <44844728+Robobo2022@users.noreply.github.com>
2022-08-12 15:39:34 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,19 +1,19 @@
-If game.PlaceId = (8884334497) then
+if game.PlaceId == 8884334497 then
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/MiningClickerSimulator.lua"), true))()
 end
 
-elseif game.PlaceId = (8884334497) then
+elseif game.PlaceId == 8884334497 then
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/MowTheLawn.lua"), true))()
 end
 
-elseif game.PlaceId = (183364845) then
+elseif game.PlaceId == 183364845 then
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/SpeedRun4.lua"), true))()
 end
 
-elseif game.PlaceId = (9498006165) then
+elseif game.PlaceId == 9498006165 then
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/TappingSimulator.lua"), true))()
 end
 
-elseif game.PlaceId = (4951858512) then
+elseif game.PlaceId == 4951858512 then
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/VictoryRace.lua"), true))()
 end
