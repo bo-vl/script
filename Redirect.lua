990210933a8aa2fb3021fa5f4f325c6c20c2a409
Bo <bo.van.laarhoven@gmail.com>
2022-08-17 21:37:16 +0200

    Update Redirect.lua

--- Redirect.lua
+++ Redirect.lua
@@ -1,11 +1,20 @@
-If game.PlaceId = (9498006165) then
+--tapping simulator
+if game.PlaceId == 9498006165 then
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/TappingSimulator.lua"), true))()
 end
-
-If game.PlaceId = (8884334497) then
+--Mining Clicker Simulator
+if game.PlaceId == 8884334497 then
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/MiningClickerSimulator.lua"), true))()
 end
-
-If game.PlaceId = (10108131074) then
+--Mow the Lawn
+if game.PlaceId == 10108131074 then
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/MowTheLawn.lua"), true))()
+end
+--Speed Run 4
+if game.PlaceId == 10108131074 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/SpeedRun4.lua"), true))()
+end
+--Victory Race
+if game.PlaceId == 10108131074 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/VictoryRace.lua"), true))()
 end
