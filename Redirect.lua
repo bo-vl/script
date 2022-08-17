a8ac2fb6f334078e4fca3e9770641adc750abf68
Bo <bo.van.laarhoven@gmail.com>
2022-08-17 21:36:48 +0200

    Create Redirect.lua

---
+++ Redirect.lua
@@ -0,0 +1,11 @@
+If game.PlaceId = (9498006165) then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/TappingSimulator.lua"), true))()
+end
+
+If game.PlaceId = (8884334497) then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/MiningClickerSimulator.lua"), true))()
+end
+
+If game.PlaceId = (10108131074) then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/MowTheLawn.lua"), true))()
+end
