ebe389403b5dee9656cc222db932f6a8ee0fc61f
Bo <bo.van.laarhoven@gmail.com>
2022-08-19 19:37:50 +0200

    Update VictoryJump.lua

---
+++ VictoryJump.lua
@@ -0,0 +1,68 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "bo's script Hub |Victory Jump|", HidePremium = false,IntroText = "Bo's Script Hub", SaveConfig = false, ConfigFolder = "OrionTest"})
+
+--values
+
+_G.Autowin = true
+
+--functions
+
+function Autowin()
+    while _G.Autowin == true do
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(10.875, 984929.625, 72.5, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        wait()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(10.875, 602596.125, 72.5, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        wait()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(10.875, 299802.875, 72.5, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        wait()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(10.875, 499996.625, 72.5, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        wait()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(10.875, 1001.75, 72.5, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        wait()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(10.875, 200498.125, 72.5, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        wait()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(10.875, 100195.875, 72.5, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        wait()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(10.875, 26092.125, 72.5, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        wait()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(10.875, 803648.625, 72.5, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        wait()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(10.875, 11026.625, 72.5, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        wait()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(10.875, 51290.625, 72.5, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        wait()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(10.875, 900736.375, 72.5, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        wait()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(10.875, 5007.75, 72.5, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        wait()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(10.875, 2500.25, 72.5, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        wait()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(10.875, 1739.25, 72.5, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        wait()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(10.875, 368, 72.5, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        wait()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(10.875, 708819.125, 72.5, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        wait()
+    end
+end
+
+--tabs
+
+local AutoFarm = Window:MakeTab({
+	Name = "AutoFarm",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+--toggles/buttons/dropdowns
+
+AutoFarm:AddToggle({
+	Name = "Auto Win",
+	Default = false,
+	Callback = function(Value)
+		_G.Autowin = Value
+        Autowin()
+	end    
+})
+
+OrionLib:Init()
