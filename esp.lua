f4348adbc4da3a07166665ea219a2c7ccf81b3c0
Bo <bo.van.laarhoven@gmail.com>
2022-08-20 14:18:32 +0200

    esp

--- esp.lua
+++ esp.lua
@@ -1,12 +1,25 @@
 local players = game.Players:GetPlayers()
 
-        for i,v in pairs(players) do
-            local esp = Instance.new("Highlight")
-            esp.Name = v.Name
-            esp.FillTransparency = 0.75
-            esp.FillColor = Color3.new(255, 0, 0)
-            esp.OutlineColor = Color3.new(0, 0, 0)
-            esp.OutlineTransparency = 0
-            esp.Parent = v.Character
-           end
-        end
+local function esp(v)
+    repeat wait() until v.Character.Humanoid
+    local esp = Instance.new("Highlight")
+    esp.Name = v.Name
+    esp.FillTransparency = 0.75
+    esp.FillColor = Color3.new(255, 0, 0)
+    esp.OutlineColor = Color3.new(0,0,0)
+    esp.OutlineTransparency = 0
+    esp.Parent = v.Character
+end
+
+for i,v in pairs(players) do
+    esp(v)
+    v.CharacterAdded:Connect(function()
+        esp(v)
+    end)
+end
+
+game.Players.PlayerAdded:Connect(function(v)
+    v.CharacterAdded:Connect(function()
+        esp(v)
+    end)
+end)
