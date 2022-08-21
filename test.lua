a72bcca121a0fa50edef2a557476a843b96ea6f3
Bo <bo.van.laarhoven@gmail.com>
2022-08-21 22:31:41 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,25 @@
-print("noob")
+local RushCoroutine = coroutine.create(function()
+    workspace.ChildAdded:Connect(function(Instance)
+        if Instance.Name == "RushMoving" then
+            local OldPos = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+            local Connect = game:GetService("RunService").Heartbeat:Connect(function()
+                game.Players.LocalPlayer.Character:MoveTo(OldPos + Vector3.new(0,20,0))
+            end)
+            
+            Instance.Destroying:Wait()
+            Connect:Disconnect()
+
+            game.Players.LocalPlayer.Character:MoveTo(OldPos)
+        end
+    elseif Instance.Name == "AmbushMoving" then
+        local OldPos = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+        local Connect = game:GetService("RunService").Heartbeat:Connect(function()
+            game.Players.LocalPlayer.Character:MoveTo(OldPos + Vector3.new(0,20,0))
+        end)
+        
+        Instance.Destroying:Wait()
+        Connect:Disconnect()
+        
+        game.Players.LocalPlayer.Character:MoveTo(OldPos)
+    end)
+end)
