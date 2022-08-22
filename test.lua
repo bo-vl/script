1bee4a095a9bb705cce79e3c42635c2466838083
Bo <bo.van.laarhoven@gmail.com>
2022-08-22 08:36:17 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,27 +1,22 @@
-local RushCoroutine = coroutine.create(function()
-    workspace.ChildAdded:Connect(function(Instance)
-        if Instance.Name == "RushMoving" then
-            local OldPos = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
-            local Connect = game:GetService("RunService").Heartbeat:Connect(function()
-                game.Players.LocalPlayer.Character:MoveTo(OldPos + Vector3.new(0,20,0))
-            end)
-            
-            Instance.Destroying:Wait()
-            Connect:Disconnect()
-
-            game.Players.LocalPlayer.Character:MoveTo(OldPos)
+local function check()
+    for i, plr in ipairs(game:GetService("Players"):GetPlayers()) do
+        if plr ~= game.Players.LocalPlayer then
+            local char = plr.Character or plr.CharacterAdded:Wait()
+            local hrp = char.HumanoidRootPart
+            for i,plr2 in ipairs(game:GetService("Players"):GetPlayers()) do
+                local otherChar = plr2.Character or plr2.CharacterAdded:Wait()
+                local otherHrp = otherChar.HumanoidRootPart
+                if (hrp.Position - otherHrp.Position).magnitude < 10 then
+                  
+local lphrp = game.Players.LocalPlayer.Character.HumanoidRootPart
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(lphrp.CFrame.X,lphrp.CFrame.Y + 10,lphrp.CFrame.Z)
+                end
+            end
         end
-    elseif Instance.Name == "AmbushMoving" then
-        local OldPos = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
-        local Connect = game:GetService("RunService").Heartbeat:Connect(function()
-            game.Players.LocalPlayer.Character:MoveTo(OldPos + Vector3.new(0,20,0))
-        end)
-        
-        Instance.Destroying:Wait()
-        Connect:Disconnect()
-        
-        game.Players.LocalPlayer.Character:MoveTo(OldPos)
-    end)
-end)
+    end
+end
 
-coroutine.resume(RushCoroutine)
+while true do
+    check()
+    wait(1)
+end
