c8ebd2a1594209103e5446881a712fe66a724d45
Bo <bo.van.laarhoven@gmail.com>
2022-08-23 23:48:14 +0200

    Update EVADE.lua

--- EVADE.lua
+++ EVADE.lua
@@ -8,23 +8,54 @@ local CharTab= Window:MakeTab({
 	PremiumOnly = false
 })
 
-CharTab:AddBind({
-	Name = "Reset HipHeigth",
-	Default = Enum.KeyCode.E,
-	Hold = false,
-	Callback = function()
-		game.Players.LocalPlayer.Character.Humanoid.HipHeight = -1
-	end    
+local TargetWalkspeed
+CharTab:AddSlider({
+	Name = "Speed",
+	Min = 0,
+	Max = 100,
+	Default = 5,
+	Color = Color3.fromRGB(255,255,255),
+	Increment = 1,
+	Callback = function(Value)
+		TargetWalkspeed = Value
+	end   
 })
 
-CharTab:AddBind({
-	Name = "Set hipheight 20",
-	Default = Enum.KeyCode.X,
-	Hold = false,
-	Callback = function()
-		game.Players.LocalPlayer.Character.Humanoid.HipHeight = 20
-	end    
+CharTab:AddSlider({
+    Name = "Hip height",
+    Min = -1.75,
+    Max = 100,
+    Default = -1.75,
+    Color = Color3.fromRGB(255,255,255),
+    Increment = 1,
+    Callback = function(HipValue)
+        game.Players.LocalPlayer.Character.Humanoid.HipHeight = HipValue
+    end    
 })
 
+--functions and shit
+
+local pcl = Instance.new("SpotLight")
+pcl.Brightness = 10
+pcl.Face = Enum.NormalId.Front
+pcl.Range = 90
+pcl.Parent = game.Players.LocalPlayer.Character.Head
+pcl.Enabled = false
+
+CharTab:AddToggle({
+	Name = "Headlight",
+	Default = false,
+    Callback = function(Value)
+        pcl.Enabled = Value
+    end
+})
+
+game:GetService("RunService").RenderStepped:Connect(function()
+    pcall(function()
+        if game.Players.LocalPlayer.Character.Humanoid.MoveDirection.Magnitude > 0 then
+            game.Players.LocalPlayer.Character:TranslateBy(game.Players.LocalPlayer.Character.Humanoid.MoveDirection * TargetWalkspeed/100)
+        end
+    end)
+end)
 
 OrionLib:Init()
