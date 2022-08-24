d538fc80d9af11115f644186079f3ac669cca92b
Bo <bo.van.laarhoven@gmail.com>
2022-08-24 11:57:22 +0200

    Update EVADE.lua

--- EVADE.lua
+++ EVADE.lua
@@ -1,11 +1,17 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "bo's script Hub |Evade|", HidePremium = false,IntroText = "Evade", SaveConfig = false, ConfigFolder = "OrionTest"})
+local GuiService = game:GetService("GuiService")
 local Light = game:GetService("Lighting")
 
 function dofullbright()
     Light.Ambient = Color3.new(1, 1, 1)
     Light.ColorShift_Bottom = Color3.new(1, 1, 1)
     Light.ColorShift_Top = Color3.new(1, 1, 1)
+    game.Lighting.FogEnd = 100000
+    game.Lighting.FogStart = 0
+    game.Lighting.ClockTime = 14
+    game.Lighting.Brightness = 2
+    game.Lighting.GlobalShadows = false
     end
 
 
@@ -15,12 +21,18 @@ local CharTab= Window:MakeTab({
 	PremiumOnly = false
 })
 
+local MiscTab= Window:MakeTab({
+	Name = "Misc",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
 local TargetWalkspeed
 CharTab:AddSlider({
 	Name = "Speed",
 	Min = 0,
 	Max = 100,
-	Default = 5,
+	Default = 0,
 	Color = Color3.fromRGB(255,255,255),
 	Increment = 1,
 	Callback = function(Value)
@@ -30,9 +42,9 @@ CharTab:AddSlider({
 
 CharTab:AddSlider({
     Name = "Hip height",
-    Min = -1.75,
+    Min = -1.50,
     Max = 100,
-    Default = -1.75,
+    Default = -1.50,
     Color = Color3.fromRGB(255,255,255),
     Increment = 1,
     Callback = function(HipValue)
@@ -40,14 +52,27 @@ CharTab:AddSlider({
     end    
 })
 
-CharTab:AddButton({
+MiscTab:AddButton({
+	Name = "Inf Jump",
+	Callback = function()
+        local InfiniteJumpEnabled = true
+        game:GetService("UserInputService").JumpRequest:connect(function()
+            if InfiniteJumpEnabled then
+                game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
+            end
+        end)
+  	end    
+})
+
+MiscTab:AddButton({
 	Name = "Full Bright",
 	Callback = function()
         dofullbright()
   	end    
 })
-
+\
 --functions and shit
+
 
 game:GetService("RunService").RenderStepped:Connect(function()
     pcall(function()
