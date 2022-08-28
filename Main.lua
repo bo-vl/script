9b81fd23ca081956b110e5c7a69cee23fd6cd58d
Bo <bo.van.laarhoven@gmail.com>
2022-08-28 14:09:18 +0200

    New game

--- Main.lua
+++ Main.lua
@@ -42,3 +42,11 @@ if game.PlaceId == 9498006165 then
  if game.PlaceId == 9872472334 or 10662542523 or 10324347967 or 10324346056 or 10539706691 then
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/EVADE.lua"), true))()
  end
+ --Nico's next bot
+ if game.PlaceId == 10118559731 then
+   loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/niconextbot.lua"), true))()
+end
+ --RainBow friends
+ if game.PlaceId == 7991339063 or 8888615802 then
+   loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/niconextbot.lua"), true))()
+end
