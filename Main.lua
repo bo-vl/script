8bfb5a86bad6cb6b6ea85fe4dbec96c363d64c9a
Bo <bo.van.laarhoven@gmail.com>
2022-09-12 16:34:34 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,69 +1,56 @@
 --tapping simulator
 if game.PlaceId == 9498006165 then
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/TappingSimulator.lua"), true))()
-end
 --Mining Clicker Simulator
-if game.PlaceId == 8884334497 then
+elseif game.PlaceId == 8884334497 then
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/MiningClickerSimulator.lua"), true))()
-end
 --Mow the Lawn
-if game.PlaceId == 10108131074 then
+elseif game.PlaceId == 10108131074 then
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/MowTheLawn.lua"), true))()
-end
 --Speed Run 4
-if game.PlaceId == 183364845 then
+elseif game.PlaceId == 183364845 then
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/SpeedRun4.lua"), true))()
-end
 --Victory Race
-if game.PlaceId == 4951858512 then
+elseif game.PlaceId == 4951858512 then
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/VictoryRace.lua"), true))()
-end
 --Speed Run Simulator
-if game.PlaceId == 5293755937 then
+elseif game.PlaceId == 5293755937 then
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/SpeedRunSimulator.lua"), true))()
-end
 --Speed Run Simulator 2
-if game.PlaceId == 7047488135 then
+elseif game.PlaceId == 7047488135 then
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/SpeedrunSimulator2.lua"), true))()
-end
 --Victory Jump
-if game.PlaceId == 10379662529 then
+elseif game.PlaceId == 10379662529 then
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/VictoryJump.lua"), true))()
-end
 --Magic Clicker
-if game.PlaceId == 9170761016 then
+elseif game.PlaceId == 9170761016 then
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/MagicClicker.lua"), true))()
-end
 --wheat Farming simulator
-if game.PlaceId == 10106105124 then
+elseif game.PlaceId == 10106105124 then
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/WheatFarmingSimulator.lua"), true))()
-end
 --Evade
-if game.PlaceId == 9872472334 then
+elseif game.PlaceId == 9872472334 then
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/EVADE.lua"), true))()
-end
 
-if game.PlaceId == 10662542523 then
+elseif game.PlaceId == 10662542523 then
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/EVADE.lua"), true))()
-end
 
-if game.PlaceId == 10324347967 then
+elseif game.PlaceId == 10324347967 then
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/EVADE.lua"), true))()
-end
 
-if game.PlaceId == 10324346056 then
+elseif game.PlaceId == 10324346056 then
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/EVADE.lua"), true))()
-end
 
-if game.PlaceId == 10530697691 then
+elseif game.PlaceId == 10530697691 then
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/EVADE.lua"), true))()
-end
 -----------
 --RaindBow friends
-if game.PlaceId == 8888615802 then
+elseif game.PlaceId == 8888615802 then
    loadstring(game:HttpGet(('https://raw.githubusercontent.com/spookysss/RainbowFriends/main/script%20(2).lua')))()
-end
 --nico's nextbots
-if game.PlaceId == 10118559731 then
+elseif game.PlaceId == 10118559731 then
    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/nico'snextbot.lua"), true))()
+--universal
+else
+   loadstring(game:HttpGet(("https://raw.githubusercontent.com/Robobo2022/script/main/Universal.lua"), true))()
 end
