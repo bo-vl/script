90dcd26cc2f81a1b2c30ddf7f4809be41c2ae757
Bo <bo.van.laarhoven@gmail.com>
2022-10-11 18:18:15 +0200

    Update prison life.lua

--- prison life.lua
+++ prison life.lua
@@ -1,3 +1,4 @@
+local Light = game:GetService("Lighting")
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Hydra Network | Prison life", HidePremium = false, IntroText = "Prison Life", SaveConfig = true, ConfigFolder = "OrionTest"})
 
@@ -33,15 +34,83 @@ MainTab:AddDropdown({
 	end    
 })
 
+local Section = MainTab:AddSection({
+	Name = "Weapon Mod"
+})
+
+MainTab:AddButton({
+	Name = "Mod M9",
+	Callback = function()
+		s = game:GetService("Players").LocalPlayer.Backpack.M9.GunStates
+		rs = require(s)
+		rs.CurrentAmmo = math.huge
+		rs.MaxAmmo = math.huge
+		rs.ReloadTime = 0
+  	end    
+})
+
+MainTab:AddButton({
+	Name = "Mod Ak-47",
+	Callback = function()
+		s = game:GetService("Players").LocalPlayer.Backpack["AK-47"].GunStates
+		rs = require(s)
+		rs.CurrentAmmo = math.huge
+		rs.MaxAmmo = math.huge
+		rs.ReloadTime = 0
+  	end    
+})
+
+MainTab:AddButton({
+	Name = "Mod Remington 870",
+	Callback = function()
+		s = game:GetService("Players").RoBobo2008.Backpack["Remington 870"].GunStates
+		rs = require(s)
+		rs.CurrentAmmo = math.huge
+		rs.MaxAmmo = math.huge
+		rs.ReloadTime = 0
+  	end    
+})
+
+MainTab:AddButton({
+	Name = "Mod Ak-47",
+	Callback = function()
+		s = game:GetService("Players").LocalPlayer.Backpack.M4A1.GunStates
+		rs = require(s)
+		rs.CurrentAmmo = math.huge
+		rs.MaxAmmo = math.huge
+		rs.ReloadTime = 0
+  	end    
+})
+
+local Section = MainTab:AddSection({
+	Name = "Other"
+})
+
+MainTab:AddButton({
+	Name = "Full Bright",
+	Callback = function()
+        Light.Ambient = Color3.new(1, 1, 1)
+        Light.ColorShift_Bottom = Color3.new(1, 1, 1)
+        Light.ColorShift_Top = Color3.new(1, 1, 1)
+        game.Lighting.FogEnd = 100000
+        game.Lighting.FogStart = 0
+        game.Lighting.ClockTime = 14
+        game.Lighting.Brightness = 10
+        game.Lighting.GlobalShadows = false
+  	end    
+})
+
+local TargetWalkspeed
 PlayerTab:AddSlider({
 	Name = "Speed",
 	Min = 0,
 	Max = 500,
-	Default = 16,
-	Color = Color3.fromRGB(255,255,255),
+	Default = 0,
+	Color = Color3.fromRGB(137, 207, 240),
 	Increment = 1,
-	Callback = function(WalkSpeed)
-        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = WalkSpeed
+	ValueName = "Walk Speed",
+	Callback = function(Value)
+		TargetWalkspeed = Value
 	end   
 })
 
@@ -51,8 +120,9 @@ PlayerTab:AddSlider({
 	Min = 0,
 	Max = 500,
 	Default = 50.145,
-	Color = Color3.fromRGB(255,255,255),
+	Color = Color3.fromRGB(137, 207, 240),
 	Increment = 1,
+	ValueName = "Jump Power",
 	Callback = function(JumpPower)
         game.Players.LocalPlayer.Character.Humanoid.JumpPower = JumpPower
     end
@@ -63,8 +133,9 @@ PlayerTab:AddSlider({
 	Min = 0,
 	Max = 500,
 	Default = 0,
-	Color = Color3.fromRGB(255,255,255),
+	Color = Color3.fromRGB(137, 207, 240),
 	Increment = 1,
+	ValueName = "Hip Height",
 	Callback = function(JumpHeight)
         game.Players.LocalPlayer.Character.Humanoid.HipHeight = JumpHeight
     end
@@ -75,8 +146,9 @@ PlayerTab:AddSlider({
 	Min = 1,
 	Max = 120,
 	Default = 70,
-	Color = Color3.fromRGB(255,255,255),
+	Color = Color3.fromRGB(137, 207, 240),
 	Increment = 1,
+	ValueName = "Fov",
 	Callback = function(Fov)
         game:GetService'Workspace'.Camera.FieldOfView = Fov
     end
@@ -87,21 +159,18 @@ PlayerTab:AddSlider({
 	Min = 0,
 	Max = 196.2,
 	Default = 196.2,
-	Color = Color3.fromRGB(255,255,255),
+	Color = Color3.fromRGB(137, 207, 240),
 	Increment = 1,
+	ValueName = "Gravity",
 	Callback = function(Gravity)
         Workspace.Gravity = Gravity
     end
 })
 
-PlayerTab:AddSlider({
-	Name = "Day & night Slider",
-	Min = 0,
-	Max = 24,
-	Default = 14,
-	Color = Color3.fromRGB(255,255,255),
-	Increment = 0.1,
-	Callback = function(Time)
-        game.Lighting.ClockTime = Time
-    end
-})
+game:GetService("RunService").RenderStepped:Connect(function()
+    pcall(function()
+        if game.Players.LocalPlayer.Character.Humanoid.MoveDirection.Magnitude > 0 then
+            game.Players.LocalPlayer.Character:TranslateBy(game.Players.LocalPlayer.Character.Humanoid.MoveDirection * TargetWalkspeed/500)
+        end
+    end)
+end)
