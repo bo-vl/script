cea73b23160891c56f60d301fc6608cd5fb85d1b
Bo <bo.van.laarhoven@gmail.com>
2022-10-23 12:31:47 +0200

    Update EVADE.lua

--- EVADE.lua
+++ EVADE.lua
@@ -18,7 +18,7 @@ OrionLib:MakeNotification({
     Image = "rbxassetid://4483345998",
     Time = 2
 })
-local Window = OrionLib:MakeWindow({Name = "Hydra Network |Evade|", HidePremium = false,IntroText = "Evade V2.6", SaveConfig = false, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "Hydra Network |Evade|", HidePremium = false,IntroText = "Evade V2.8", SaveConfig = false, ConfigFolder = "OrionTest"})
 local ownerId = game.CreatorId
 game.Players.PlayerAdded:Connect(function(player)
     if player.UserId == ownerId then
@@ -610,6 +610,33 @@ MiscTab:AddButton({
         local ohString1 = "ThirdPerson"
         local ohBoolean2 = true
         game:GetService("Players").LocalPlayer.PlayerScripts.Events.KeybindUsed:Fire(ohString1, ohBoolean2)
+  	end    
+})
+
+MainTab:AddButton({
+	Name = "Ticket esp",
+	Callback = function()
+        while true do
+            repeat wait() until game.Workspace.Game.Effects.Tickets.Ticket1
+            local BillboardGui = Instance.new("BillboardGui")
+            local TextLabel = Instance.new("TextLabel")
+            BillboardGui.Parent = game.Workspace.Game.Effects.Tickets.Ticket1
+            BillboardGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+            BillboardGui.Active = true
+            BillboardGui.AlwaysOnTop = true
+            BillboardGui.ExtentsOffset = Vector3.new(0, 3, 0)
+            BillboardGui.LightInfluence = 1.000
+            BillboardGui.Size = UDim2.new(0, 200, 0, 50)
+            TextLabel.Parent = BillboardGui
+            TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+            TextLabel.Size = UDim2.new(0, 200, 0, 50)
+            TextLabel.Font = Enum.Font.SourceSans
+            TextLabel.Text = "Ticket"
+            TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+            TextLabel.TextSize = 14.000
+            TextLabel.TextScaled = true
+            TextLabel.BackgroundTransparency = 1
+        end
   	end    
 })
 
