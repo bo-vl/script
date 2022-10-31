b35a8061237a8a87008a1a056a5d6c1ba7f633a8
Bo <bo.van.laarhoven@gmail.com>
2022-10-31 18:28:54 +0100

    Update EVADE.lua

--- EVADE.lua
+++ EVADE.lua
@@ -1,4 +1,3 @@
-getgenv().SecureMode = true
 game:GetService("ReplicatedStorage").Events.Respawn:FireServer()
 wait(1)
 local WorkspacePlayers = game:GetService("Workspace").Game.Players
@@ -39,6 +38,14 @@ getgenv().Settings = {
     reviveTime = 3,
     TicketFarm = false,
 }
+
+local FindAI = function()
+    for _,v in pairs(WorkspacePlayers:GetChildren()) do
+        if not Players:FindFirstChild(v.Name) then
+            return v
+        end
+    end
+end
 
 local GetDownedPlr = function()
     for i,v in pairs(WorkspacePlayers:GetChildren()) do
@@ -354,6 +361,96 @@ local Button = T2:CreateButton({
 	end,
 })
 
+local Button = T3:CreateButton({
+	Name = "Player Esp",
+	Callback = function()
+        local dwEntities = game:GetService("Players")
+        local dwLocalPlayer = dwEntities.LocalPlayer 
+        local dwRunService = game:GetService("RunService")
+        
+        local settings_tbl = {
+            ESP_Enabled = true,
+            ESP_TeamCheck = false,
+            Chams = true,
+            Chams_Color = Color3.fromRGB(255, 255, 255),
+            Chams_Transparency = 0.9,
+            Chams_Glow_Color = Color3.fromRGB(0, 0, 0)
+        }
+        
+        function destroy_chams(char)
+            for k,v in next, char:GetChildren() do 
+                if v:IsA("BasePart") and v.Transparency ~= 1 then
+                    if v:FindFirstChild("Glow") and 
+                    v:FindFirstChild("Chams") then
+                        v.Glow:Destroy()
+                        v.Chams:Destroy() 
+                    end 
+                end 
+            end 
+        end
+        
+        dwRunService.Heartbeat:Connect(function()
+            if settings_tbl.ESP_Enabled then
+                for k,v in next, dwEntities:GetPlayers() do 
+                    if v ~= dwLocalPlayer then
+                        if v.Character and
+                        v.Character:FindFirstChild("HumanoidRootPart") and 
+                        v.Character:FindFirstChild("Humanoid") and 
+                        v.Character:FindFirstChild("Humanoid").Health ~= 0 then
+                            if settings_tbl.ESP_TeamCheck == false then
+                                local char = v.Character 
+                                for k,b in next, char:GetChildren() do 
+                                    if b:IsA("BasePart") and 
+                                    b.Transparency ~= 1 then
+                                        if settings_tbl.Chams then
+                                            if not b:FindFirstChild("Glow") and
+                                            not b:FindFirstChild("Chams") then
+                                                local chams_box = Instance.new("BoxHandleAdornment", b)
+                                                chams_box.Name = "Chams"
+                                                chams_box.AlwaysOnTop = true 
+                                                chams_box.ZIndex = 4 
+                                                chams_box.Adornee = b 
+                                                chams_box.Color3 = settings_tbl.Chams_Color
+                                                chams_box.Transparency = settings_tbl.Chams_Transparency
+                                                chams_box.Size = b.Size + Vector3.new(0.02, 0.02, 0.02)
+                                                local glow_box = Instance.new("BoxHandleAdornment", b)
+                                                glow_box.Name = "Glow"
+                                                glow_box.AlwaysOnTop = false 
+                                                glow_box.ZIndex = 3 
+                                                glow_box.Adornee = b 
+                                                glow_box.Color3 = settings_tbl.Chams_Glow_Color
+                                                glow_box.Size = chams_box.Size + Vector3.new(0.13, 0.13, 0.13)
+                                            end
+                                        else
+                                            destroy_chams(char)
+                                        end
+                                    end
+                                end
+                            else
+                                if v.Team == dwLocalPlayer.Team then
+                                    destroy_chams(v.Character)
+                                end
+                            end
+                        else
+                            destroy_chams(v.Character)
+                        end
+                    end
+                end
+            else 
+                for k,v in next, dwEntities:GetPlayers() do 
+                    if v ~= dwLocalPlayer and 
+                    v.Character and 
+                    v.Character:FindFirstChild("HumanoidRootPart") and 
+                    v.Character:FindFirstChild("Humanoid") and 
+                    v.Character:FindFirstChild("Humanoid").Health ~= 0 then
+                        destroy_chams(v.Character)
+                    end
+                end
+            end
+        end)
+	end,
+})
+
 local Button = T2:CreateButton({
 	Name = "Inf Jump",
 	Callback = function()
@@ -365,6 +462,7 @@ local Button = T2:CreateButton({
         end)
 	end,
 })
+
 
 local Button = T2:CreateButton({
 	Name = "T to teleport",
