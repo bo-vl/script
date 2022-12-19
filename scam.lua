45740d8127bd6f93d224f8d4d71c8cd4a8952ca8
Bo <bo.van.laarhoven@gmail.com>
2022-12-20 00:48:28 +0100

    Update scam.lua

--- scam.lua
+++ scam.lua
@@ -1,12 +1,12 @@
 repeat wait() until game:IsLoaded()
 
 for i,v in pairs(game:GetService("Workspace"):GetDescendants()) do
-    if v.Name == "Claim" and v.ClassName == "Part" and v.Transparency == 0 then
-        game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
-    end
+   if v.Name == "Claim" and v.ClassName == "Part" and v.Transparency == 0 then
+      game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
+   end
 end
 
-game:GetService("Players").LocalPlayer.PlayerGui.Notifications:Destroy()
+--game:GetService("Players").LocalPlayer.PlayerGui.Notifications:Destroy()
 game:GetService("Players").LocalPlayer.PlayerGui.PCGUI.Frame.Upgrades.Update:Fire()
 game:GetService("ReplicatedStorage").Events.GenerateNumber:FireServer()
 wait(2)
@@ -39,19 +39,79 @@ local Window = Rayfield:CreateWindow({
     }
  })
 
-local Tab = Window:CreateTab("Auto Farm", 4483362458)
+local Tab = Window:CreateTab("Auto Farm", 11866433904)
 
 getgenv().AutoFarm = false
+getgenv().FasterScamming = false
+getgenv().TransferSpeed = false
+getgenv().ScamValue = false
+
+local TransferSpeed = game:GetService("Players").LocalPlayer.Data.TransferSpeed.Value
+local fasterscamming = game:GetService("Players").LocalPlayer.Data.ScamSpeedLevel.Value
+local transferspeed = game:GetService("Players").LocalPlayer.Data.TransferSpeedLevel.Value
+local scamvalue = game:GetService("Players").LocalPlayer.Data.ScamValueLevel.Value
 
 task.spawn(function()
-	while task.wait() do
+   while task.wait() do
+      if FasterScamming == true then
+         local ohString1 = "ScamSpeed"
+         local ohNumber2 = (fasterscamming)
+         game:GetService("ReplicatedStorage").Events.UpgradeItem:FireServer(ohString1, ohNumber2)
+      end
+   end
+end)
+
+task.spawn(function()
+   while task.wait() do
+      if TransferSpeed == true then
+         local ohString1 = "TransferSpeed"
+         local ohNumber2 = (transferspeed)
+         game:GetService("ReplicatedStorage").Events.UpgradeItem:FireServer(ohString1, ohNumber2)
+      end
+   end
+end)
+
+task.spawn(function()
+   while task.wait() do
+      if ScamValue == true then
+         local ohString1 = "ScamValue"
+         local ohNumber2 = (scamvalue)
+         game:GetService("ReplicatedStorage").Events.UpgradeItem:FireServer(ohString1, ohNumber2)
+      end
+   end
+end)
+
+
+task.spawn(function()
+	while task.wait(TransferSpeed) do
 		if getgenv().AutoFarm == true then
             local number = game:GetService("Players").LocalPlayer.GeneratedNumber.Value
             local ohNumber1 = (number)
             game:GetService("ReplicatedStorage").Events.CheckNumber:FireServer(ohNumber1)
+
+            wait(5)
+            if number == number then
+               Rayfield:Notify({
+                  Title = "Generated Number",
+                  Content = "Generated Number: " .. number,
+                  Duration = 6.5,
+                  Image = 11866433904,
+               })
+            elseif number ~= number then
+               print("same number")
+               
+            end
+
         end
 	end
 end)
+
+
+
+local ohString1 = "ScamSpeed"
+local ohNumber2 = 4
+
+game:GetService("ReplicatedStorage").Events.UpgradeItem:FireServer(ohString1, ohNumber2)
 
 local Toggle = Tab:CreateToggle({
    Name = "Scam call Auto Farm",
@@ -63,6 +123,39 @@ local Toggle = Tab:CreateToggle({
    end,
 })
 
+local Label = Tab:CreateLabel("Upgrade")
+
+local Toggle = Tab:CreateToggle({
+   Name = "Faster Scamming",
+   Info = "Auto buys the upgrade called 'Faster Scamming'",
+   CurrentValue = false,
+   Flag = "Toggle1",
+   Callback = function(Value)
+      FasterScamming = Value
+   end,
+})
+
+local Toggle = Tab:CreateToggle({
+   Name = "Transfer Speed",
+   Info = "Auto buys the upgrade called 'Transfer Speed'",
+   CurrentValue = false,
+   Flag = "Toggle1",
+   Callback = function(Value)
+      TransferSpeed = Value
+   end,
+})
+
+local Toggle = Tab:CreateToggle({
+   Name = "Scam Value",
+   Info = "Auto buys the upgrade called 'Scam Value'",
+   CurrentValue = false,
+   Flag = "Toggle1",
+   Callback = function(Value)
+      ScamValue = Value
+   end,
+})
+
+local Label = Tab:CreateLabel("Other")
 local Toggle = Tab:CreateToggle({
    Name = "Turn on Sprint",
    Info = "Disables sprinting buy promt",
@@ -85,6 +178,3 @@ local Button = Tab:CreateButton({
       game:GetService("Players").LocalPlayer.Data.DoubleMoney.Value = math.huge
    end,
 })
-
-
-
