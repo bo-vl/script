f772e26fcd9958477da534474db947a8d1d734c9
Bo <bo.van.laarhoven@gmail.com>
2022-12-25 23:23:17 +0100

    Update EVADE.lua

--- EVADE.lua
+++ EVADE.lua
@@ -33,7 +33,7 @@ local Window = Rayfield:CreateWindow({
 		FileName = "SiriusKey",
 		SaveKey = false,
 		GrabKeyFromSite = false,
-		Key = "sdjkfhlksjdhflksjhadlfkjhd"
+		Key = "HH6NEYnZ5vp8zZjzMtcX"
 	}
 })
 
