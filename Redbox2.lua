be88ab8a0b846d83b04a9069a01aec7bc7fc8d24
Bo <bo.van.laarhoven@gmail.com>
2022-12-28 21:02:03 +0100

    Update Redbox2.lua

--- Redbox2.lua
+++ Redbox2.lua
@@ -2,257 +2,288 @@ local StarterPlayer = game:GetService("StarterPlayer")
 local Workspace = game:GetService("Workspace")
 local Light = game:GetService("Lighting")
 local players = game.Players:GetPlayers()
-local repo = 'https://raw.githubusercontent.com/wally-rblx/LinoriaLib/main/'
-local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
-local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
-local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
-local ownerId = game.CreatorId
-game.Players.PlayerAdded:Connect(function(player)
-    if player.UserId == ownerId then
-        game.Players.LocalPlayer:Kick("Owner Joined")
-    end
-end)
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 
-Library:SetWatermark('Hydra Network | Redbox2 v2.0')
-
+local Window = Rayfield:CreateWindow({
+	Name = "Redbox",
+	LoadingTitle = "Hydra Network",
+	LoadingSubtitle = "by hydra#8270",
+	ConfigurationSaving = {
+		Enabled = true,
+		FolderName = nil,
+		FileName = "Big Hub"
+	},
+        Discord = {
+        	Enabled = true,
+        	Invite = "YvwEyH2W6t",
+        	RememberJoins = true 
+        },
+	KeySystem = false,
+	KeySettings = {
+		Title = "Hydra Network",
+		Subtitle = "Key System",
+		Note = "Key link copied in clipboard!",
+		FileName = "SiriusKey",
+		SaveKey = false,
+		GrabKeyFromSite = true,
+		Key = "https://pastebin.com/raw/KcaMdP0i"
+	}
+})
 getgenv().Color = BrickColor
-getgenv().HeadSize = 10
-getgenv().Rootpart = 50
+getgenv().HeadSize = 1
+getgenv().Rootpart = 2
 getgenv().Disabled = false
 
 game:GetService('RunService').RenderStepped:connect(function()
-    if Disabled then
-    for i,v in next, game:GetService('Players'):GetPlayers() do
-    if v.Name ~= game:GetService('Players').LocalPlayer.Name then
-    pcall(function()
-    v.Character.Head.Size = Vector3.new(HeadSize,HeadSize,HeadSize)
-    v.Character.Head.Transparency = 0.2
-    v.Character.Head.BrickColor = BrickColor.new(Color)
-    v.Character.Head.Material = "ForceField"
-    v.Character.Head.CanCollide = false
-    end)
+    if Disabled == false then
+        for i,v in next, game:GetService('Players'):GetPlayers() do
+            if v.Name ~= game:GetService('Players').LocalPlayer.Name then
+                pcall(function()
+                    v.Character.Head.Size = Vector3.new(1,1,1)
+                    v.Character.HumanoidRootPart.Size = Vector3.new(2,2,2)
+                    v.Character.Head.Transparency = 0
+                    v.Character.HumanoidRootPart.Transparency = 0
+                    v.Character.Head.Material = "ForceField"
+                    v.Character.HumanoidRootPart.Material = "Plastic"
+                end)
+            end
+        end
     end
-    end
-    end
-    end)
+end)
 
 game:GetService('RunService').RenderStepped:connect(function()
     if Disabled then
-    for i,v in next, game:GetService('Players'):GetPlayers() do
-    if v.Name ~= game:GetService('Players').LocalPlayer.Name then
-    pcall(function()
-    v.Character.HumanoidRootPart.Size = Vector3.new(Rootpart,Rootpart,Rootpart)
-    v.Character.HumanoidRootPart.Transparency = 0.2
-    v.Character.HumanoidRootPart.BrickColor = BrickColor.new(Color)
-    v.Character.HumanoidRootPart.Material = "ForceField"
-    v.Character.HumanoidRootPart.CanCollide = false
-    end)
+        for i,v in next, game:GetService('Players'):GetPlayers() do
+            if v.Name ~= game:GetService('Players').LocalPlayer.Name then
+                pcall(function()
+                    v.Character.Head.Size = Vector3.new(HeadSize,HeadSize,HeadSize)
+                    v.Character.Head.Transparency = 0.2
+                    v.Character.Head.BrickColor = BrickColor.new(Color)
+                    v.Character.Head.Material = "ForceField"
+                    v.Character.Head.CanCollide = false
+                end)
+            end
+        end
     end
-    end
-    end
-    end)
-
-local Window = Library:CreateWindow({
-    Title = 'RedBox2 v2',
-    Center = true, 
-    AutoShow = true,
-})
-local Tabs = {
-    Main = Window:AddTab('Hitbox'), 
-    Main1 = Window:AddTab('Local Player'), 
-    ['UI Settings'] = Window:AddTab('UI Settings'),
-}
-local Sliders = Tabs.Main:AddLeftGroupbox('Hitbox Sliders')
-local ColorWheel = Tabs.Main:AddRightGroupbox('Color Wheel')
-local Hitbox = Tabs.Main:AddRightGroupbox('Toggles')
-local Modifieres = Tabs.Main1:AddLeftGroupbox('Slider Modifieres')
-local Modifieres1 = Tabs.Main1:AddLeftGroupbox('Keybind Modifieres')
-local Modifieres2 = Tabs.Main1:AddRightGroupbox('Button Modifieres')
-
-Sliders:AddSlider('HitboxHead', {
-    Text = 'Hitbox Slider (head)',
-    Default = 0,
-    Min = 0,
-    Max = 5,
-    Rounding = 1,
-    Compact = false,
-})
-
-Options.HitboxHead:OnChanged(function()
-    HeadSize = Options.HitboxHead.Value
 end)
 
-Sliders:AddSlider('HitboxRoot', {
-    Text = 'Hitbox Slider (HumanoidRootPart)',
-    Default = 2,
-    Min = 2,
-    Max = 100,
-    Rounding = 1,
-    Compact = false,
-})
-
-Options.HitboxRoot:OnChanged(function()
-    Rootpart = Options.HitboxRoot.Value
+game:GetService('RunService').RenderStepped:connect(function()
+    if Disabled then
+        for i,v in next, game:GetService('Players'):GetPlayers() do
+            if v.Name ~= game:GetService('Players').LocalPlayer.Name then
+                pcall(function()
+                    v.Character.HumanoidRootPart.Size = Vector3.new(Rootpart,Rootpart,Rootpart)
+                    v.Character.HumanoidRootPart.Transparency = 0.2
+                    v.Character.HumanoidRootPart.BrickColor = BrickColor.new(Color)
+                    v.Character.HumanoidRootPart.Material = "ForceField"
+                    v.Character.HumanoidRootPart.CanCollide = false
+                end)
+            end
+        end
+    end
 end)
 
+local T1 = Window:CreateTab("Main")
+local T2 = Window:CreateTab("Player")
 
-ColorWheel:AddLabel('Hitbox Color'):AddColorPicker('ColorPicker', {
-    Default = Color3.new(0, 1, 0),
-    Title = 'Hitbox Color',
+local Keybind = T1:CreateKeybind({
+    Name = "Inf ammo(equipe Weapon before pressing)",
+    CurrentKeybind = "R",
+    HoldToInteract = false,
+    Callback = function(Keybind)
+        local Tool = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Tool")
+        local m = require(Tool.toolSettings)
+        game:GetService("Players").LocalPlayer.Character.Humanoid:UnequipTools()
+        game:GetService("Players").LocalPlayer.Character.Humanoid:EquipTool(Tool)
+        game:GetService("ReplicatedStorage").Assets.Remotes.syncAmmo:FireServer(1000000,1000000, Tool)
+    end,
 })
 
-Options.ColorPicker:OnChanged(function()
-    Color = Options.ColorPicker.Value
-end)
+
+
+local Keybind = T1:CreateKeybind({
+    Name = "No Recoil",
+    CurrentKeybind = "L",
+    HoldToInteract = false,
+    Callback = function(Keybind)
+        local Tool = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Tool")
+        local m = require(Tool.toolSettings)
+        m.firingRecoilEnabled = false
+    end,
+})
+
+
+local Button = T2:CreateButton({
+	Name = "Fullbright",
+	Callback = function()
+        Light.Ambient = Color3.new(1, 1, 1)
+        Light.ColorShift_Bottom = Color3.new(1, 1, 1)
+        Light.ColorShift_Top = Color3.new(1, 1, 1)
+        game.Lighting.FogEnd = 100000
+        game.Lighting.FogStart = 0
+        game.Lighting.Brightness = 5
+        game.Lighting.GlobalShadows = false
+	end,
+})
+
+local Slider = T1:CreateSlider({
+	Name = "Hitbox (Head)",
+	Range = {0, 5},
+	Increment = 1,
+	Suffix = "HeadSize",
+	CurrentValue = 0,
+	Callback = function(Value)
+        HeadSize = Value
+	end,
+})
+
+local Slider = T1:CreateSlider({
+	Name = "Hitbox (HumanoidRootPart)",
+	Range = {0, 50},
+	Increment = 1,
+	Suffix = "HumanoidRootPartSize",
+	CurrentValue = 0,
+	Callback = function(Value)
+        Rootpart = Value
+	end,
+})
+
+local Toggle = T1:CreateToggle({
+	Name = "Hitbox Toggle",
+	CurrentValue = false,
+	Callback = function(Value)
+        Disabled = Value
+	end,
+})
+
+local Slider = T2:CreateSlider({
+	Name = "Fov Slider",
+	Range = {70, 120},
+	Increment = 1,
+	Suffix = "Fov",
+	CurrentValue = 70,
+	Callback = function(Value)
+        game:GetService'Workspace'.Camera.FieldOfView = Value
+	end,
+})
 
 local TargetWalkspeed
-Modifieres:AddSlider('Speed', {
-    Text = 'Speed slider',
-    Default = 0,
-    Min = 0,
-    Max = 500,
-    Rounding = 1,
-    Compact = false,
+local Slider = T2:CreateSlider({
+	Name = "WalkSpeed Slider",
+	Range = {0, 500},
+	Increment = 1,
+	Suffix = "WalkSpeed",
+	CurrentValue = 0,
+	Callback = function(Value)
+        TargetWalkspeed = Value
+	end,
 })
 
-Options.Speed:OnChanged(function()
-    TargetWalkspeed = Options.Speed.Value
-end)
-
-Modifieres:AddSlider('Jump', {
-    Text = 'Jump slider',
-    Default = 0,
-    Min = 0,
-    Max = 250,
-    Rounding = 1,
-    Compact = false,
+local Slider = T2:CreateSlider({
+	Name = "JumpPower Slider",
+	Range = {50, 500},
+	Increment = 1,
+	Suffix = "JumpPower",
+	CurrentValue = 50,
+	Callback = function(Value)
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+	end,
 })
 
-Options.Jump:OnChanged(function()
-    game.Players.LocalPlayer.Character.Humanoid.JumpPower = Options.Jump.Value
-end)
-
-Modifieres:AddSlider('Fov', {
-    Text = 'Fov slider',
-    Default = 70,
-    Min = 0,
-    Max = 120,
-    Rounding = 1,
-    Compact = false,
-})
-
-Options.Fov:OnChanged(function()
-    game:GetService'Workspace'.Camera.FieldOfView = Options.Fov.Value
-end)
-
-Modifieres1:AddLabel('Inf Ammo'):AddKeyPicker('KeyPicker', {
-    Default = 'R',
-    SyncToggleState = false, 
-    Mode = 'Hold',
-    Text = 'Inf Ammo',
-    NoUI = false,
-})
-
-Options.KeyPicker:OnClick(function()
-    KeyPicker:GetState()
-end)
-
-
-task.spawn(function()
-    while true do
-        wait(0.1)
-        local state = Options.KeyPicker:GetState()
-        if state then
-            local Tool = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Tool")
-            local m = require(Tool.toolSettings)
-            game:GetService("Players").LocalPlayer.Character.Humanoid:UnequipTools()
-            game:GetService("Players").LocalPlayer.Character.Humanoid:EquipTool(Tool)
-            game:GetService("ReplicatedStorage").Assets.Remotes.syncAmmo:FireServer(1000000,1000000, Tool)
+local Button = T1:CreateButton({
+	Name = "Player esp",
+	Callback = function()
+        local dwEntities = game:GetService("Players")
+        local dwLocalPlayer = dwEntities.LocalPlayer 
+        local dwRunService = game:GetService("RunService")
+        
+        local settings_tbl = {
+            ESP_Enabled = true,
+            ESP_TeamCheck = false,
+            Chams = true,
+            Chams_Color = Color3.fromRGB(255, 255, 255),
+            Chams_Transparency = 0.9,
+            Chams_Glow_Color = Color3.fromRGB(0, 0, 0)
+        }
+        
+        function destroy_chams(char)
+            for k,v in next, char:GetChildren() do 
+                if v:IsA("BasePart") and v.Transparency ~= 1 then
+                    if v:FindFirstChild("Glow") and 
+                    v:FindFirstChild("Chams") then
+                        v.Glow:Destroy()
+                        v.Chams:Destroy() 
+                    end 
+                end 
+            end 
         end
-        if Library.Unloaded then break end
-    end
-end)
-
-Modifieres1:AddLabel('No Recoil'):AddKeyPicker('NoRecoil', {
-    Default = 'T',
-    SyncToggleState = false, 
-    Mode = 'Hold',
-    Text = 'No Recoil',
-    NoUI = false,
+        
+        dwRunService.Heartbeat:Connect(function()
+            if settings_tbl.ESP_Enabled then
+                for k,v in next, dwEntities:GetPlayers() do 
+                    if v ~= dwLocalPlayer then
+                        if v.Character and
+                        v.Character:FindFirstChild("HumanoidRootPart") and 
+                        v.Character:FindFirstChild("Humanoid") and 
+                        v.Character:FindFirstChild("Humanoid").Health ~= 0 then
+                            if settings_tbl.ESP_TeamCheck == false then
+                                local char = v.Character 
+                                for k,b in next, char:GetChildren() do 
+                                    if b:IsA("BasePart") and 
+                                    b.Transparency ~= 1 then
+                                        if settings_tbl.Chams then
+                                            if not b:FindFirstChild("Glow") and
+                                            not b:FindFirstChild("Chams") then
+                                                local chams_box = Instance.new("BoxHandleAdornment", b)
+                                                chams_box.Name = "Chams"
+                                                chams_box.AlwaysOnTop = true 
+                                                chams_box.ZIndex = 4 
+                                                chams_box.Adornee = b 
+                                                chams_box.Color3 = settings_tbl.Chams_Color
+                                                chams_box.Transparency = settings_tbl.Chams_Transparency
+                                                chams_box.Size = b.Size + Vector3.new(0.02, 0.02, 0.02)
+                                                local glow_box = Instance.new("BoxHandleAdornment", b)
+                                                glow_box.Name = "Glow"
+                                                glow_box.AlwaysOnTop = false 
+                                                glow_box.ZIndex = 3 
+                                                glow_box.Adornee = b 
+                                                glow_box.Color3 = settings_tbl.Chams_Glow_Color
+                                                glow_box.Size = chams_box.Size + Vector3.new(0.13, 0.13, 0.13)
+                                            end
+                                        else
+                                            destroy_chams(char)
+                                        end
+                                    end
+                                end
+                            else
+                                if v.Team == dwLocalPlayer.Team then
+                                    destroy_chams(v.Character)
+                                end
+                            end
+                        else
+                            destroy_chams(v.Character)
+                        end
+                    end
+                end
+            else 
+                for k,v in next, dwEntities:GetPlayers() do 
+                    if v ~= dwLocalPlayer and 
+                    v.Character and 
+                    v.Character:FindFirstChild("HumanoidRootPart") and 
+                    v.Character:FindFirstChild("Humanoid") and 
+                    v.Character:FindFirstChild("Humanoid").Health ~= 0 then
+                        destroy_chams(v.Character)
+                    end
+                end
+            end
+        end)
+	end,
 })
 
-Options.NoRecoil:OnClick(function()
-    NoRecoil:GetState()
-end)
-
-task.spawn(function()
-    while true do
-        wait(0.1)
-        local state = Options.NoRecoil:GetState()
-        if state then
-            local Tool = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Tool")
-            local m = require(Tool.toolSettings)
-            m.firingRecoilEnabled = false
-        end
-        if Library.Unloaded then break end
-    end
-end)
-
-local MyButton = Modifieres2:AddButton('Full Bright', function()
-    Light.Ambient = Color3.new(1, 1, 1)
-    Light.ColorShift_Bottom = Color3.new(1, 1, 1)
-    Light.ColorShift_Top = Color3.new(1, 1, 1)
-    game.Lighting.FogEnd = 100000
-    game.Lighting.FogStart = 0
-    game.Lighting.Brightness = 5
-    game.Lighting.GlobalShadows = false
-end)
-
-local MyButton = Modifieres2:AddButton('Turn off watermark', function()
-    Library:SetWatermarkVisibility(false)
-end)
-
-local MyButton = Modifieres2:AddButton('Turn on watermark', function()
-    Library:SetWatermarkVisibility(true)
-end)
-
-local MyButton = Modifieres2:AddButton('Turn on Keybind Gui', function()
-    Library.KeybindFrame.Visible = true;
-end)
-
-local MyButton = Modifieres2:AddButton('Turn off Keybind Gui', function()
-    Library.KeybindFrame.Visible = false;
-end)
-
-Hitbox:AddToggle('Disa', {
-    Text = 'Hitbox Toggle',
-    Default = false,
-    Tooltip = 'Toggles the hitbox',
-})
-
-Toggles.Disa:OnChanged(function(Value)
-    Disabled = Value
-end)
-
---some functions
 game:GetService("RunService").RenderStepped:Connect(function()
     pcall(function()
         if game.Players.LocalPlayer.Character.Humanoid.MoveDirection.Magnitude > 0 then
-            game.Players.LocalPlayer.Character:TranslateBy(game.Players.LocalPlayer.Character.Humanoid.MoveDirection * TargetWalkspeed/250)
+            game.Players.LocalPlayer.Character:TranslateBy(game.Players.LocalPlayer.Character.Humanoid.MoveDirection * TargetWalkspeed/100)
         end
     end)
 end)
-
---settings
-local MenuGroup = Tabs['UI Settings']:AddLeftGroupbox('Menu')
-MenuGroup:AddButton('Unload', function() Library:Unload() end)
-MenuGroup:AddLabel('Menu bind'):AddKeyPicker('MenuKeybind', { Default = 'End', NoUI = true, Text = 'Menu keybind' }) 
-Library.ToggleKeybind = Options.MenuKeybind
-ThemeManager:SetLibrary(Library)
-SaveManager:SetLibrary(Library)
-SaveManager:IgnoreThemeSettings() 
-SaveManager:SetIgnoreIndexes({ 'MenuKeybind' }) 
-ThemeManager:SetFolder('MyScriptHub')
-SaveManager:SetFolder('MyScriptHub/specific-game')
-SaveManager:BuildConfigSection(Tabs['UI Settings']) 
-ThemeManager:ApplyToTab(Tabs['UI Settings'])
