8dbe4af6f9ccfab6b77db7a08607181973b77cf0
Bo <bo.van.laarhoven@gmail.com>
2023-01-02 21:13:52 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -54,7 +54,8 @@ local GameTable = {
     [5431104427] = "https://raw.githubusercontent.com/Robobo2022/script/main/%20Notoriety.lua",
     [5891659534] = "https://raw.githubusercontent.com/Robobo2022/script/main/%20Notoriety.lua",
     [10605039538] = "https://raw.githubusercontent.com/Robobo2022/script/main/%20Notoriety.lua",
-    [6537140247] = "https://raw.githubusercontent.com/Robobo2022/script/main/%20Notoriety.lua"
+    [6537140247] = "https://raw.githubusercontent.com/Robobo2022/script/main/%20Notoriety.lua",
+    [2317712696] = "https://raw.githubusercontent.com/Robobo2022/script/main/thewildwest.lua"
 }
 
 
