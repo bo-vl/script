38c2695675058cfc8816ebc6f3fd4170db295ce8
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-03-12 00:45:29 +0100

    ok

--- Main.lua
+++ Main.lua
@@ -56,8 +56,6 @@ local GameTable = {
     [10605039538] = "https://raw.githubusercontent.com/Robobo2022/script/main/%20Notoriety.lua",
     [6537140247] = "https://raw.githubusercontent.com/Robobo2022/script/main/%20Notoriety.lua",
     [2317712696] = "https://raw.githubusercontent.com/Robobo2022/script/main/thewildwest.lua",
-    [2413927524] = "https://raw.githubusercontent.com/Robobo2022/script/main/Rake.lua",
-    [8908228901] = "https://raw.githubusercontent.com/Robobo2022/script/main/sharkbite2.lua"
 }
 
 
@@ -80,3 +78,47 @@ if GameTable[game.PlaceId] == nil then
 else
     loadstring(game:HttpGet(GameTable[game.PlaceId]))()
 end
+
+
+for _,v in next, getconnections(game:GetService("ScriptContext").Error) do
+    v:Disable()
+end
+
+for _,v in next, getconnections(game:GetService("LogService").MessageOut) do
+    v:Disable()
+end
+
+local Time = os.clock()
+local ownerId = game.CreatorId
+local Lib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/Robobo2022/notify-lib/main/lib'),true))()
+if getgenv().Loaded then    
+    Lib.prompt('Hydra Network v4', 'Hydra Network v4 has already been executed', 2)
+    return
+end
+
+getgenv().Loaded = true
+game.Players.PlayerAdded:Connect(function(player)
+    if player.UserId == ownerId then
+        Lib.prompt('Hydra Network v4', 'Owned Joined the game', 2)
+    end
+end)
+
+local games = ({
+    [8908228901] = "sharkbite2.lua",
+    [9872472334] = "EVADE.lua",
+    [10662542523] = "EVADE.lua",
+    [10324347967] = "EVADE.lua",
+    [10530697691] = "EVADE.lua",
+    [10530697691] = "EVADE.lua",
+    [10324346056] = "EVADE.lua",
+    [10808838353] = "EVADE.lua",
+})[game.PlaceId]
+
+if games then
+    Lib.prompt('Game is supported, and is now executing ' .. games, 2)
+    print("Time taken: " .. os.clock() - Time)
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Robobo2022/script/tree/main/Games/" .. games))()
+else
+    Lib.prompt('Hydra Network v4', 'Game is not supported, and is now executing universal', 2)
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Robobo2022/script/main/Games/Universal.lua", true))();
+end 
