ebff5293ac91591f08524fdbd5ebe0e9dfcf8c90
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-04-12 18:47:17 +0200

    Update Project-Validus.lua

--- Project-Validus.lua
+++ Project-Validus.lua
@@ -6,6 +6,7 @@ for _, v in pairs(getconnections(game:GetService("LogService").MessageOut)) do
     v:Disable()
 end
 
+local Request = (syn and syn.request or request or http and http.request or http_request) or error("No request function")
 local plrs = game:GetService('Players')
 local lplr = plrs.LocalPlayer
 local mouse = lplr:GetMouse()
@@ -921,6 +922,33 @@ MenuGroup:AddToggle('Watermark', {
 Toggles.Watermark:OnChanged(function()
     Library:SetWatermarkVisibility(Toggles.Watermark.Value)
 end)
+
+local MyButton = MenuGroup:AddButton({
+    Text = 'Join Discord',
+    Func = function()
+        Request(
+            {
+                Url = "http://127.0.0.1:6463/rpc?v=1",
+                Method = "POST",
+                Headers = {
+                    ["Content-Type"] = "application/json",
+                    ["origin"] = "https://discord.com",
+                },
+                Body = game:GetService("HttpService"):JSONEncode(
+                    {
+                        ["args"] = {
+                            ["code"] = "YvwEyH2W6t",
+                        },
+                        ["cmd"] = "INVITE_BROWSER",
+                        ["nonce"] = "."
+                    })
+            })
+    end,
+    DoubleClick = false,
+    Tooltip = 'makes you join the discord server'
+})
+
+
 
 Library.ToggleKeybind = Options.MenuKeybind
 ThemeManager:SetLibrary(Library)
